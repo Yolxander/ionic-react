4ea07bd2d90b858c4695e9b1e072f38e445727b9
Yolxi <jacacanada@gmail.com>
2020-09-10 17:43:02 -0400

    added localstorge usage

--- src/App.js
+++ src/App.js
@@ -11,11 +11,12 @@ import {
 } from '@ionic/react';
 import React, { useState } from 'react';
 import BiorhythmCard from './components/BiorhythmCard';
+import { useLocalStorage }from './hooks'
 
 
 
 function App() {
-  const [birthDate, setBirthDate] = useState('');
+  const [birthDate, setBirthDate] = useLocalStorage('birthDate', '');
   const[targetDate,setTargetDate] = useState(new Date().toISOString());
 
 
